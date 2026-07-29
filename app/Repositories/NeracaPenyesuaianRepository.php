d0f96758199cbe78a2166ad18190830670496d77
Eko Permana <permanaeko74@gmail.com>
2026-07-29 14:07:22 +0700

    fix: laba

--- app/Repositories/NeracaPenyesuaianRepository.php
+++ app/Repositories/NeracaPenyesuaianRepository.php
@@ -9,11 +9,11 @@ class NeracaPenyesuaianRepository
     /**
      * Dapatkan total nilai penyesuaian neraca.
      */
-public function getTotalPenyesuaian($month, $year, $tokoId): float
-{
-    return (float) NeracaPenyesuaian::where('toko_id', $tokoId)
-        ->whereMonth('tanggal', $month)
-        ->whereYear('tanggal', $year)
-        ->sum('nominal');
-}
+    public function getTotalPenyesuaian($month, $year, $tokoId): float
+    {
+        return (float) NeracaPenyesuaian::where('toko_id', $tokoId)
+            ->whereMonth('tanggal', $month)
+            ->whereYear('tanggal', $year)
+            ->sum('nominal');
+    }
 }

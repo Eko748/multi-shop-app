63b09b72c4c5b7c3f9f9b0378891ce5cd0a49d15
Eko Permana <permanaeko74@gmail.com>
2026-08-03 16:39:01 +0700

    fix

--- app/Http/Controllers/Distribusi/PlanOrderController.php
+++ app/Http/Controllers/Distribusi/PlanOrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Distribusi;
 
+use App\Helpers\TextGenerate;
 use App\Http\Controllers\Controller;
 use App\Models\Barang;
 use App\Models\PengirimanBarangDetail;
@@ -118,7 +119,7 @@ class PlanOrderController extends Controller
 
                 return [
                     'id' => $item->id,
-                    'nama_barang' => $item->nama,
+                    'nama_barang' => TextGenerate::smartTail($item->nama),
                     'grand_total_stock' => $grandTotalStock,
                     'stok_per_toko' => $stokPerToko,
                 ];

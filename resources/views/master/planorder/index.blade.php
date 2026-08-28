65ca3049ebacedc2e33cb16d96fe6375cfe27f92
Eko Permana <permanaeko74@gmail.com>
2026-08-28 12:37:14 +0700

    feat: dashboard v3

--- resources/views/master/planorder/index.blade.php
+++ resources/views/master/planorder/index.blade.php
@@ -320,7 +320,7 @@
                 return `
                     <th class="text-wrap align-top text-center sortable-col header-${index}-stock ${activeStock}"
                     data-sort-by="stock" data-sort-toko="${key}" title="Urutkan Stok ${key}"
-                    style="background: linear-gradient(to bottom, #a8e6a1, #66ff66); width: 1px; min-width: 30px; cursor: pointer;">
+                    style="background: linear-gradient(to bottom, #a8e6a1, #66ff66); width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px; cursor: pointer;">
                     <div class="d-flex align-items-center justify-content-center">
                         <i class="fa fa-box"></i>
                         ${getSortIcon('stock')}
@@ -328,7 +328,7 @@
                 </th>
                 <th class="text-wrap align-top text-center sortable-col header-${index}-otw ${activeOtw}"
                     data-sort-by="otw" data-sort-toko="${key}" title="Urutkan OTW ${key}"
-                    style="background: linear-gradient(to bottom, #fff9a1, #ffff33); width: 1px; min-width: 30px; cursor: pointer;">
+                    style="background: linear-gradient(to bottom, #fff9a1, #ffff33); width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px; cursor: pointer;">
                     <div class="d-flex align-items-center justify-content-center">
                         <i class="fa fa-truck-fast"></i>
                         ${getSortIcon('otw')}
@@ -336,7 +336,7 @@
                 </th>
                 <th class="text-wrap align-top text-center sortable-col header-${index}-lo ${activeLo}"
                     data-sort-by="lo" data-sort-toko="${key}" title="Urutkan Last Order ${key}"
-                    style="background: linear-gradient(to bottom, #a1e9ff, #00ccff); width: 1px; min-width: 30px; cursor: pointer;">
+                    style="background: linear-gradient(to bottom, #a1e9ff, #00ccff); width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px; cursor: pointer;">
                     <div class="d-flex align-items-center justify-content-center">
                         <i class="fa fa-clock"></i>
                         ${getSortIcon('lo')}
@@ -364,9 +364,9 @@
                 let stokColumns = dynamicKeys.map((key, i) => {
                     let tokoData = element.stok_per_toko[key] || {};
                     return `
-                        <td class="${classCol} text-center header-${i}-stock" style="background-color: #CCFFCC"><b>${tokoData.stock ?? '-'}</b></td>
-                        <td class="${classCol} text-center header-${i}-otw" style="background-color: #FFFFCC"><b>${tokoData.otw ?? '-'}</b></td>
-                        <td class="${classCol} text-center header-${i}-lo" style="background-color: #99CCFF"><b>${tokoData.lo !== null ? tokoData.lo + ' hr' : '-'}</b></td>
+                        <td class="${classCol} text-center header-${i}-stock" style="background-color: #CCFFCC; width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px;"><b>${tokoData.stock ?? '-'}</b></td>
+                        <td class="${classCol} text-center header-${i}-otw" style="background-color: #FFFFCC; width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px;"><b>${tokoData.otw ?? '-'}</b></td>
+                        <td class="${classCol} text-center header-${i}-lo" style="background-color: #99CCFF; width: 1px; white-space: nowrap; padding-left: 4px; padding-right: 4px;"><b>${tokoData.lo !== null ? tokoData.lo + ' hr' : '-'}</b></td>
                     `;
                 }).join('');
 
